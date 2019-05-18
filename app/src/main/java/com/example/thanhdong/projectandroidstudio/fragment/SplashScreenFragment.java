db90636705f105a21c1aae18dac0f6b27374ac26
thanhdong272 <dongthanh272@gmail.com>
2019-05-18 10:47:15 +0700

    1. Refactor follow comment pull request.

--- app/src/main/java/com/example/thanhdong/projectandroidstudio/fragment/SplashScreenFragment.java
+++ app/src/main/java/com/example/thanhdong/projectandroidstudio/fragment/SplashScreenFragment.java
@@ -28,6 +28,8 @@ import com.example.thanhdong.projectandroidstudio.R;
  * create an instance of this fragment.
  */
 public class SplashScreenFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
